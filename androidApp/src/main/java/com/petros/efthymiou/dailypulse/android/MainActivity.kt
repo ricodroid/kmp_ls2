3d2d86eb88911c5f5b3caf4a7913dbd72b39922d
r_murata <r_murata@avelio.co.jp>
2024-03-29 11:56:33 +0900

    ジェットパックコンポーズを排除できた

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.petros.efthymiou.dailypulse.android
 
 import android.os.Bundle
-import androidx.activity.viewModels
 
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,37 +8,25 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.petros.efthymiou.dailypulse.articles.application.Article
 import com.petros.efthymiou.dailypulse.articles.presentation.ArticlesViewModel
-
+import com.petros.efthymiou.dailypulse.sources.presentation.SourcesViewModel
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
-
+    private val articlesViewModel: ArticlesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val articleList = listOf(
-            Article(
-                "てきすと",
-                "Futures were higher in premarket trading as Wall Street tried to regain its footing.",
-                "2023-11-09",
-                "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
-            ),
-            Article(
-                "テキスト2",
-                "Apple's smartphones rarely go on sale, but if you’re looking to upgrade (or you're gift shopping), here are a few cost-saving options.",
-                "2023-11-09",
-                "https://media.wired.com/photos/622aa5c8cca6acf55fb70b57/191:100/w_1280,c_limit/iPhone-13-Pro-Colors-SOURCE-Apple-Gear.jpg",
-            ),
-            Article(
-                "テキスト3",
-                "In a new blog post, Samsung previewed what it calls “a new era of Galaxy AI” coming to its smartphones and detailed a feature that will use artificial intelligence to translate phone calls in real time.",
-                "2023-11-09",
-                "https://cdn.vox-cdn.com/thumbor/Ocz_QcxUdtaexp1pPTMygaqzbR8=/0x0:2000x1333/1200x628/filters:focal(1000x667:1001x668)/cdn.vox-cdn.com/uploads/chorus_asset/file/24396795/DSC04128_processed.jpg",
-            ),
-        )
+        val articlesState = articlesViewModel.articlesState.value
+        if (articlesState.error != null) {
+            // ここにステータスの色々を入れる
+        }
+
+        val articleList = articlesState.articles
 
         recyclerView = findViewById(R.id.RecyclerList)
         recyclerView.adapter = RecyclerAdapter(articleList)
--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/RecyclerAdapter.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/RecyclerAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.petros.efthymiou.dailypulse.articles.application.Article
+import com.petros.efthymiou.dailypulse.sources.presentation.SourcesState
+import kotlinx.coroutines.flow.StateFlow
 
 class RecyclerAdapter(val articleList: List<Article>) : RecyclerView.Adapter<ViewHolderList>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderList {
