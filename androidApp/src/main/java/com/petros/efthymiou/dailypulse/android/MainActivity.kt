7145c2c4e36f2b6f27d853ed81f70174773d3ed3
r_murata <r_murata@avelio.co.jp>
2024-03-29 11:42:37 +0900

    リストデータをベタ書き

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
@@ -14,14 +14,32 @@ import com.petros.efthymiou.dailypulse.articles.presentation.ArticlesViewModel
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
-    val articlesViewModel: ArticlesViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val articleList = articlesViewModel.articlesState.value.articles
-
+        val articleList = listOf(
+            Article(
+                "てきすと",
+                "Futures were higher in premarket trading as Wall Street tried to regain its footing.",
+                "2023-11-09",
+                "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
+            ),
+            Article(
+                "テキスト2",
+                "Apple's smartphones rarely go on sale, but if you’re looking to upgrade (or you're gift shopping), here are a few cost-saving options.",
+                "2023-11-09",
+                "https://media.wired.com/photos/622aa5c8cca6acf55fb70b57/191:100/w_1280,c_limit/iPhone-13-Pro-Colors-SOURCE-Apple-Gear.jpg",
+            ),
+            Article(
+                "テキスト3",
+                "In a new blog post, Samsung previewed what it calls “a new era of Galaxy AI” coming to its smartphones and detailed a feature that will use artificial intelligence to translate phone calls in real time.",
+                "2023-11-09",
+                "https://cdn.vox-cdn.com/thumbor/Ocz_QcxUdtaexp1pPTMygaqzbR8=/0x0:2000x1333/1200x628/filters:focal(1000x667:1001x668)/cdn.vox-cdn.com/uploads/chorus_asset/file/24396795/DSC04128_processed.jpg",
+            ),
+        )
 
         recyclerView = findViewById(R.id.RecyclerList)
         recyclerView.adapter = RecyclerAdapter(articleList)
